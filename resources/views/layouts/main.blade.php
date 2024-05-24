79fc4efa96917805f1cbce8146474d0ef977343c
FernandJerico <fernand.jerico.ti.20@cic.ac.id>
2024-05-24 18:00:02 +0700

    refactor: move header code to partials

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -17,20 +17,7 @@
 
 <body>
     {{-- Navbar --}}
-    <header>
-        <img src="{{ asset('images/logo-text-dark.png') }}" alt="logo" class="logo">
-        <nav>
-            <ul class="nav-links">
-                <li><a href="#">Landing Page</a></li>
-                <li><a href="#">Eksplorasi</a></li>
-                <li><a href="#">Tentang Kami</a></li>
-            </ul>
-            <div class="auth">
-                <a href="#" class="btn-register">Daftar</a>
-                <a href="#" class="btn-login">Masuk</a>
-            </div>
-        </nav>
-    </header>
+    @include('partials.header')
 
     <section>
         @yield('section')
