7f7149b50b145204c3befc6043d01eb97f1c59e9
FernandJerico <fernand.jerico.ti.20@cic.ac.id>
2024-05-24 21:35:41 +0700

    chore: Update plugin-api-version to 2.3.0, refactor header styling, and add active class to nav links

---
+++ resources/views/pages/exploration.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.main')
+
+@section('section')
+    <div class="exploration">
+        <h1>Eksplorasi</h1>
+    </div>
+@endsection
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -2,9 +2,10 @@
     <img src="{{ asset('images/logo-text-dark.png') }}" alt="logo" class="logo">
     <nav>
         <ul class="nav-links">
-            <li><a href="#">Landing Page</a></li>
-            <li><a href="#">Eksplorasi</a></li>
-            <li><a href="#">Tentang Kami</a></li>
+            <li class="nav-item {{ Request::is('/', '/*') ? 'active' : '' }}"><a href="/">Landing Page</a></li>
+            <li class="nav-item {{ Request::is('eksplorasi', '/eksplorasi*') ? 'active' : '' }}"><a
+                    href="{{ route('eksplorasi') }}">Eksplorasi</a></li>
+            <li class="nav-item"><a href="#">Tentang Kami</a></li>
         </ul>
         <div class="auth">
             <a href="#" class="btn-register">Daftar</a>
